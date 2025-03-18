660c120c8c3ec9627920cb0c58a5b6f76433c32a
eddie <eddiezal28@gmail.com>
2025-03-18 13:39:45 -0700

    migrate map components to feature-based architecture2

--- src/features/map/components/MapView.tsx
+++ src/features/map/components/MapView.tsx
@@ -1,29 +1,4 @@
-// src/features/map/components/MapView.tsx
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useAppContext } from '../../../core/context/AppContext';
 import { useMapInteractions } from '../hooks/useMapInteractions';
-import { X, Star, Clock } from 'lucide-react';
 import { CATEGORY_COLORS } from '../constants';
-
-const MapView: React.FC = () => {
-  const mapContainerRef = useRef<HTMLDivElement>(null);
-  const { filteredProducers, selectedProducer, setSelectedProducer } = useAppContext();
-  
-  const {
-    isLoading,
-    isGoogleMapsLoaded,
-    loadGoogleMapsApi,
-    createMarkers,
-    // Other functions from the hook
-  } = useMapInteractions();
-  
-  // Rest of the component implementation, using the hook functions
-  
-  return (
-    <div className="relative h-full w-full">
-      {/* UI implementation */}
-    </div>
-  );
-};
-
-export default MapView;
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,16 +1,12 @@
 // src/pages/Map.tsx
 import React from 'react';
-import MapView from '../components/Map/MapView';
+import MapView from '../features/map/components/MapView';
 import { producers } from '../data/mockProducers';
 
 const Map: React.FC = () => {
   return (
     <div className="h-full">
-      <MapView 
-        producers={producers} 
-        selectedCategory="all" 
-        filterAvailability="now" 
-      />
+      <MapView />
     </div>
   );
 };
