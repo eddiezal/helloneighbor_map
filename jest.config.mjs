d11669744328143e05904ac478130bece7d8f034
eddie <eddiezal28@gmail.com>
2025-03-18 22:43:50 -0700

    Update testing configuration and fix components for better test coverage

--- jest.config.mjs
+++ jest.config.mjs
@@ -13,11 +13,6 @@ export default {
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/__tests__/**/*.test.(ts|tsx)', '**/__tests__/**/?(*.)(spec|test).[jt]s?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-      isolatedModules: true
-    }
-  }
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+
 };
